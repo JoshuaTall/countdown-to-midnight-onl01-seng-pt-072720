66f0e025ba7dce687e663afa010f0c2937dc5560
Joshua Tall <tall.joshua1984@yahoo.com>
2020-07-31 02:34:56 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,13 +1,6 @@
-def countdown
-  x = 10
-  while countdown < 11
-    countdown -= 10
-    puts "#{x} SECOND(S)!"
-  end
-  while countdown == 0
-    puts "HAPPY NEW YEAR"
-  end
-end
+def countdown_with_sleep
+  
+ 
 
 #def countdown
 #end
